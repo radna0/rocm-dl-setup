b61703df75f7cafd742ead4c3450ae8a865b1af7
HGB <87762857+radna0@users.noreply.github.com>
2024-05-16 07:46:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,140 +1,116 @@
 #!/bin/bash
 
+# Update system and install essential packages
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    ca-certificates \
+    nano \
+    wget \
+    curl \
+    gnupg \
+    ripgrep \
+    ltrace \
+    file \
+    python3-minimal \
+    build-essential \
+    git \
+    cmake \
+    ninja-build \
+    python3-pip
+
+# Set environment variables
+export PATH="${PATH}:/opt/rocm/bin:/opt/rocm/llvm/bin:/usr/local/cuda/bin/"
+export NVIDIA_VISIBLE_DEVICES=all
+export NVIDIA_DRIVER_CAPABILITIES=compute,utility
+
+# Install CUDA
+CUDA_VERSION="11-8"
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    nvidia-headless-no-dkms-515 \
+    nvidia-utils-515 \
+    cuda-cudart-${CUDA_VERSION} \
+    cuda-compiler-${CUDA_VERSION} \
+    libcufft-dev-${CUDA_VERSION} \
+    libcusparse-dev-${CUDA_VERSION} \
+    libcublas-dev-${CUDA_VERSION} \
+    cuda-nvml-dev-${CUDA_VERSION} \
+    libcudnn8-dev \
+    cuda-toolkit-${CUDA_VERSION} \
+    cudnn9-cuda-${CUDA_VERSION}
+    
+# Install Rust
+RUST_VERSION="1.66.1"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y --default-toolchain=${RUST_VERSION}
+source $HOME/.cargo/env
+cargo install bindgen-cli --locked
+
+# Install ROCm
+ROCM_VERSION="5.7.1"
+echo "Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600" | sudo tee /etc/apt/preferences.d/rocm-pin-600
+sudo mkdir -p /etc/apt/keyrings
+sudo wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | sudo gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/${ROCM_VERSION} jammy main" | sudo tee /etc/apt/sources.list.d/rocm.list
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    rocminfo \
+    rocm-gdb \
+    rocprofiler \
+    rocm-smi-lib \
+    hip-runtime-amd \
+    comgr \
+    hipblaslt-dev \
+    hipfft-dev \
+    rocblas-dev \
+    rocsolver-dev \
+    rocsparse-dev \
+    miopen-hip-dev \
+    rocm-device-libs
+echo 'export PATH="$PATH:/opt/rocm/bin"' | sudo tee /etc/profile.d/rocm.sh
+echo '/opt/rocm/lib' | sudo tee /etc/ld.so.conf.d/rocm.conf
+sudo ldconfig
+
+# Install Zluda
+git clone --recurse-submodules https://github.com/vosen/ZLUDA.git $HOME/ZLUDA
+cd $HOME/ZLUDA
+$HOME/.cargo/bin/cargo xtask --release
 
 
-# Function to update and install packages if needed
-update_and_install() {
-    sudo apt update -y && sudo apt upgrade -y
-    sudo apt install -y --install-suggests ca-certificates nano wget curl python3-minimal build-essential git cmake ninja-build
-}
-
-set_environment_variables() {
-    export PATH="${PATH}:/opt/rocm/bin:/opt/rocm/llvm/bin:/usr/local/cuda/bin/"
-}
-
-
-# Function to install Rust
-install_rust() {
-    if ! command -v rustup &> /dev/null; then
-        RUST_VERSION=1.66.1
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=${RUST_VERSION}
-        source $HOME/.cargo/env && cargo install bindgen-cli --locked
-        sudo apt install -y rustc
-    else
-        echo "Rust is already installed."
-    fi
-}
-
-# Function to install ROCM packages
-install_rocm_packages() {
-    if [ -d "/opt/rocm" ]; then
-        echo "ROCM packages are already installed."
-    else
-        ROCM_VERSION=5.7.1
-        echo "Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600" | sudo tee /etc/apt/preferences.d/rocm-pin-600
-        sudo mkdir -p /etc/apt/keyrings
-        sudo apt install -y "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
-        sudo apt update -y && sudo apt upgrade -y
-        wget https://repo.radeon.com/amdgpu-install/${ROCM_VERSION}/ubuntu/jammy/amdgpu-install_5.7.50701-1_all.deb
-        sudo apt install -y ./amdgpu-install_5.7.50701-1_all.deb
-        sudo apt update -y && sudo apt upgrade -y
-        sudo amdgpu-install --usecase=workstation,rocm,opencl
-        echo 'export PATH="$PATH:/opt/rocm/bin"' | sudo tee /etc/profile.d/rocm.sh
-        echo '/opt/rocm/lib' | sudo tee /etc/ld.so.conf.d/rocm.conf
-        sudo ldconfig
-    fi
-}
-
-# Function to install CUDA Toolkit
-install_cuda() {
-    if [ ! -d "/usr/local/cuda" ]; then
-        # Set CUDA version
-        CUDA_VERSION=11-8
-
-        # Install NVIDIA CUDA packages
-        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-        sudo dpkg -i cuda-keyring_1.0-1_all.deb
-        sudo apt update -y && sudo apt upgrade -y
-        sudo apt install -y --no-install-recommends \
-         nvidia-headless-no-dkms-515 \
-         nvidia-utils-515 \
-         cuda-cudart-${CUDA_VERSION} \
-         cuda-compiler-${CUDA_VERSION} \
-         libcufft-dev-${CUDA_VERSION} \
-         libcusparse-dev-${CUDA_VERSION} \
-         libcublas-dev-${CUDA_VERSION} \
-         cuda-nvml-dev-${CUDA_VERSION} \
-         libcudnn8-dev
-        wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-        sudo sh cuda_11.8.0_520.61.05_linux.run --silent --toolkit --toolkitpath=/usr/local/cuda
-    else
-        echo "CUDA is already installed."
-    fi
-}
-
-# Function to install Anaconda
-install_anaconda() {
-    if [ ! -d "$HOME/anaconda3" ]; then
-        curl -O https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
-        chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh
-        bash Anaconda3-2023.09-0-Linux-x86_64.sh -b -p $HOME/anaconda3
-        export PATH="$HOME/anaconda3/bin:$PATH"
-        source $HOME/anaconda3/etc/profile.d/conda.sh
-        source ~/.bashrc
-        conda init bash
-        conda info
-        conda update conda
-    else
-        echo "Anaconda is already installed."
-    fi
-}
-
-# Function to install Zluda
-install_zluda() {
-    if [ ! -d "$HOME/ZLUDA/target/release" ]; then
-        git clone https://github.com/vosen/ZLUDA.git $HOME/ZLUDA
-        cd $HOME/ZLUDA
-        cargo xtask --release
-    else
-        echo "Zluda is already installed."
-    fi
-}
-
-# Function to install PyTorch
-install_pytorch() {
-    if [ ! -d "$HOME/pytorch" ]; then
-        git clone https://github.com/pytorch/pytorch $HOME/pytorch
-        cd $HOME/pytorch
-        git submodule sync
-        git submodule update --init --recursive
-        export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-        export TORCH_CUDA_ARCH_LIST="6.1+PTX"
-        export CUDAARCHS=61
-        export CMAKE_CUDA_ARCHITECTURES=61
-        export USE_SYSTEM_NCCL=1
-        export USE_NCCL=0
-        export USE_EXPERIMENTAL_CUDNN_V8_API=OFF
-        export DISABLE_ADDMM_CUDA_LT=1
-        export USE_ROCM=0
-        LD_LIBRARY_PATH="$HOME/ZLUDA/target/release:$LD_LIBRARY_PATH" python setup.py develop
-    else
-        echo "PyTorch is already installed."
-    fi
-}
-
+# Install Miniconda
 cd $HOME
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+echo 'export PATH="$HOME/miniconda3:$PATH"' >> $HOME/.bashrc
+source $HOME/miniconda3/etc/profile.d/conda.sh
+source $HOME/.bashrc
+conda init
+conda --version
 
-# Run functions
-update_and_install
-set_environment_variables
-install_rocm_packages
-install_rust
-install_cuda
-install_anaconda
-install_zluda
-install_pytorch
+# Install PyTorch
+git clone --recursive https://github.com/pytorch/pytorch $HOME/pytorch
+cd $HOME/pytorch
+git submodule sync
+git submodule update --init --recursive
+conda install -y cmake ninja
+pip install -r requirements.txt
+export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+export TORCH_CUDA_ARCH_LIST="6.1+PTX"
+export CUDAARCHS=61
+export CMAKE_CUDA_ARCHITECTURES=61
+export USE_SYSTEM_NCCL=1
+export USE_NCCL=0
+export USE_EXPERIMENTAL_CUDNN_V8_API=OFF
+export DISABLE_ADDMM_CUDA_LT=1
+LD_LIBRARY_PATH="$HOME/ZLUDA/target/release:$LD_LIBRARY_PATH" python3 setup.py develop
 
+# Cleanup
 cd $HOME
+sudo rm -rf cuda-keyring_1.0-1_all.deb Miniconda3-latest-Linux-x86_64.sh
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
 
-# Set default command
-bash -l
+# Default to a login shell
+sudo bash -l
