c24577d72f0631f699626db128e6bdfac1d56d95
HGB <87762857+radna0@users.noreply.github.com>
2024-05-17 17:30:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
 RUST_VERSION="1.66.1"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y --default-toolchain=${RUST_VERSION}
 source $HOME/.cargo/env
+sudo apt install rustc cargo
 cargo install bindgen-cli --locked
 
 # Install ROCm
@@ -73,11 +74,6 @@ echo 'export PATH="$PATH:/opt/rocm/bin"' | sudo tee /etc/profile.d/rocm.sh
 echo '/opt/rocm/lib' | sudo tee /etc/ld.so.conf.d/rocm.conf
 sudo ldconfig
 
-# Install Zluda
-git clone --recurse-submodules https://github.com/vosen/ZLUDA.git $HOME/ZLUDA
-cd $HOME/ZLUDA
-cargo xtask --release
-
 
 # Install Miniconda
 cd $HOME
@@ -90,22 +86,6 @@ source $HOME/.bashrc
 conda init
 conda --version
 
-# Install PyTorch
-git clone --recursive https://github.com/pytorch/pytorch $HOME/pytorch
-cd $HOME/pytorch
-git submodule sync
-git submodule update --init --recursive
-conda install -y cmake ninja
-pip install -r requirements.txt
-export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-export TORCH_CUDA_ARCH_LIST="6.1+PTX"
-export CUDAARCHS=61
-export CMAKE_CUDA_ARCHITECTURES=61
-export USE_SYSTEM_NCCL=1
-export USE_NCCL=0
-export USE_EXPERIMENTAL_CUDNN_V8_API=OFF
-export DISABLE_ADDMM_CUDA_LT=1
-LD_LIBRARY_PATH="$HOME/ZLUDA/target/release:$LD_LIBRARY_PATH" python3 setup.py develop
 
 # Cleanup
 cd $HOME
